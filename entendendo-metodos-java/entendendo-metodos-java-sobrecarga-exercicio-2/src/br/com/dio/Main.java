c94f3d7d470b779d40a07dae33e641971bd6a981
AllanRichard <allanrichard_silva@hotmail.com>
2021-12-14 20:02:23 -0300

    commit exercicio 2 sobrecarga

---
+++ entendendo-metodos-java/entendendo-metodos-java-sobrecarga-exercicio-2/src/br/com/dio/Main.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Exercicio Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+    }
+}
